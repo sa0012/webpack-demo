c64f3999bc9b52d6ad0991cc0e9019422d567753
chenlong <1583479514@qq.com>
2020-04-24 08:57:29 +0800

    update

--- demo1/config/index.js
+++ demo1/config/index.js
@@ -1,8 +1,39 @@
+const path = require('path')
+
 module.exports = {
-  dev: {
-    title: 'dev环境'
-  },
   build: {
-    title: '生产环境'
+    env: {
+      NODE_ENV: 'production'
+    },
+    // 打包资源输出目录
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    // 静态资源存放目录
+    assetsSubDirectory: 'static',
+    // 文件输出目录， 默认在dist下， 也可以配置包裹多层目录
+    assetsPublicPath: '/',
+    // 可用来配置CDN目录
+    CDN: '/',
+    // sourceMap模式， 项目中一般情况下， 生产环境使用 source-map
+    // 开发环境使用 cheap-module-eval-source-map
+    productionSourceMap: true,
+  },
+  dev: {
+    env: {
+      NODE_ENV: 'development'
+    },
+    port: '3000',
+    productionSourceMap: false,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    devServer: {
+      '/h5-api': {
+        target: 'https://baidu.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/h5-api': '/'
+        }
+      }
+    }
   }
 }
--- demo1/dist/lib/js/test.js
+++
@@ -1,6 +0,0 @@
-function Student (name, age) {
-  this.name = name
-  this.age = age
-}
-
-let stu = new Student('jack', 23)
